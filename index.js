7245b1e1dc22c900b50f254c9ea0e375ec597eba
Frank Stokes <frank@wearereasonablepeople.com>
2019-08-23 09:38:31 +0200

    Code for eipsode

---
+++ index.js
@@ -0,0 +1,45 @@
+const zero = '+[]';
+const one = '+!![]';
+
+const number = n => {
+  if (n === 0) return zero;
+  return Array.from({length: n}, () => one).join(' + ');
+}
+
+// C
+
+const map = {};
+
+const fromString = s =>s.split('').map(x => {
+  if (!(x in map)) {
+    const charCode = x.charCodeAt(0);
+    return `([]+[])[${fromString('constructor')}][${fromString('fromCharCode')}](${number(charCode)})`;
+  }
+  return map[x];
+}).join('+');
+
+map.a = `(+{}+[])[${number(1)}]`;
+map.b = `({}+[])[${number(2)}]`;
+map.o = `({}+[])[${number(1)}]`;
+map.e = `({}+[])[${number(4)}]`;
+map.c = `({}+[])[${number(5)}]`;
+map.t = `({}+[])[${number(6)}]`;
+map[' '] = `({}+[])[${number(7)}]`;
+map.f = `(![]+[])[${number(0)}]`;
+map.s = `(![]+[])[${number(3)}]`;
+map.r = `(!![]+[])[${number(1)}]`;
+map.u = `(!![]+[])[${number(2)}]`;
+map.i = `((+!![]/+[])+[])[${number(3)}]`;
+map.n = `((+!![]/+[])+[])[${number(4)}]`;
+map.S = `([]+([]+[])[${fromString('constructor')}])[${number(9)}]`;
+map.g = `([]+([]+[])[${fromString('constructor')}])[${number(14)}]`;
+map.p = `([]+(/-/)[${fromString('constructor')}])[${number(14)}]`;
+map['\\'] = `(/\\\\/+[])[${number(1)}]`;
+map.d = `(${number(13)})[${fromString('toString')}](${number(14)})`;
+map.h = `(${number(17)})[${fromString('toString')}](${number(18)})`;
+map.m = `(${number(22)})[${fromString('toString')}](${number(23)})`;
+map.C = `((()=>{})[${fromString('constructor')}](${fromString('return escape')})()(${map['\\']}))[${number(2)}]`;
+
+const compile = code => `(()=>{})[${fromString('constructor')}](${fromString(code)})()`;
+
+console.log(compile('console.log("Hello world!");'));
