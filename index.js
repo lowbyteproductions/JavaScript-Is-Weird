f5fe01e2ebba58982b56528ece761b86a70c857e
Arjix <53124886+ArjixWasTaken@users.noreply.github.com>
2022-07-16 15:15:56 +0300

    some optimizations

--- index.js
+++ index.js
@@ -1,22 +1,49 @@
-const zero = '+[]';
-const one = '+!![]';
+const zero = "+[]";
+const one = "+!![]";
+const two = "(!![]+ !![])";
+const three = "(!![]+ !![]+ !![])";
 
-const number = n => {
-  if (n === 0) return zero;
-  return Array.from({length: n}, () => one).join(' + ');
-}
+const number = (n) => {
+    const toNum = (num) => {
+        if (num == 0) return zero;
+        if (num == 2) return two;
+        if (num == 3) return three;
+        if (num == 6) return `${two} * ${three}`;
+        if (num == 9) return `${three} * ${three}`;
 
-// C
+        return Array.from({ length: num }, () => one).join("+ ");
+    };
+
+    if (n < 10) return toNum(n);
+    const length = (n + []).length;
+    const digits = (n + [])
+        .split("")
+        .map((digit) => toNum(+digit))
+        .map((digit) => `(${digit}+[])`)
+        .map((digit, index) => {
+            if (index == length - 1) return digit;
+            return digit + "+";
+        })
+        .join("");
+
+    return `+(${digits})`;
+};
 
 const map = {};
 
-const fromString = s =>s.split('').map(x => {
-  if (!(x in map)) {
-    const charCode = x.charCodeAt(0);
-    return `([]+[])[${fromString('constructor')}][${fromString('fromCharCode')}](${number(charCode)})`;
-  }
-  return map[x];
-}).join('+');
+const fromString = (s) =>
+    s
+        .split("")
+        .map((x) => {
+            if (!(x in map)) {
+                const charCode = x.charCodeAt(0);
+                return `([]+[])[${fromString("constructor")}][${fromString("fromCharCode")}](${number(charCode)})`;
+            }
+            return map[x];
+        })
+        .join("+");
+
+const compile = (code) => `(()=>{})[${fromString("constructor")}](${fromString(code)})()`;
 
 map.a = `(+{}+[])[${number(1)}]`;
 map.b = `({}+[])[${number(2)}]`;
@@ -24,22 +51,20 @@ map.o = `({}+[])[${number(1)}]`;
 map.e = `({}+[])[${number(4)}]`;
 map.c = `({}+[])[${number(5)}]`;
 map.t = `({}+[])[${number(6)}]`;
-map[' '] = `({}+[])[${number(7)}]`;
+map[" "] = `({}+[])[${number(7)}]`;
 map.f = `(![]+[])[${number(0)}]`;
 map.s = `(![]+[])[${number(3)}]`;
 map.r = `(!![]+[])[${number(1)}]`;
 map.u = `(!![]+[])[${number(2)}]`;
 map.i = `((+!![]/+[])+[])[${number(3)}]`;
 map.n = `((+!![]/+[])+[])[${number(4)}]`;
-map.S = `([]+([]+[])[${fromString('constructor')}])[${number(9)}]`;
-map.g = `([]+([]+[])[${fromString('constructor')}])[${number(14)}]`;
-map.p = `([]+(/-/)[${fromString('constructor')}])[${number(14)}]`;
-map['\\'] = `(/\\\\/+[])[${number(1)}]`;
-map.d = `(${number(13)})[${fromString('toString')}](${number(14)})`;
-map.h = `(${number(17)})[${fromString('toString')}](${number(18)})`;
-map.m = `(${number(22)})[${fromString('toString')}](${number(23)})`;
-map.C = `((()=>{})[${fromString('constructor')}](${fromString('return escape')})()(${map['\\']}))[${number(2)}]`;
-
-const compile = code => `(()=>{})[${fromString('constructor')}](${fromString(code)})()`;
+map.S = `([]+([]+[])[${fromString("constructor")}])[${number(9)}]`;
+map.g = `([]+([]+[])[${fromString("constructor")}])[${number(14)}]`;
+map.p = `([]+(/-/)[${fromString("constructor")}])[${number(14)}]`;
+map["\\"] = `(/\\\\/+[])[${number(1)}]`;
+map.d = `(${number(13)})[${fromString("toString")}](${number(14)})`;
+map.h = `(${number(17)})[${fromString("toString")}](${number(18)})`;
+map.m = `(${number(22)})[${fromString("toString")}](${number(23)})`;
+map.C = `((()=>{})[${fromString("constructor")}](${fromString("return escape")})()(${map["\\"]}))[${number(2)}]`;
 
 console.log(compile('console.log("Hello world!");'));
